b25fb7069a691c8f4091b3f1e57d10b91a2edda3
markmumba <markmumba01@gmail.com>
2024-06-10 14:27:09 +0300

    added the foreign key constraints and added the controllers for the different models

--- backend/models/feedback.go
+++ backend/models/feedback.go
@@ -1,13 +1,15 @@
 package models
 
 import (
-	"gorm.io/gorm"
+    "gorm.io/gorm"
 )
 
 type Feedback struct {
-	gorm.Model
-	SubmissionID uint   `json:"submission_id"`
-	LecturerID   uint   `json:"lecturer_id"`
-	FeedbackDate string `json:"feedback_date"`
-	Comments     string `json:"comments"`
+    gorm.Model
+    SubmissionID uint   `json:"submission_id"`
+    LecturerID   uint   `json:"lecturer_id"`
+    FeedbackDate string `json:"feedback_date"`
+    Comments     string `json:"comments"`
+    Submission   Submission `gorm:"foreignKey:SubmissionID"`
+    Lecturer     User       `gorm:"foreignKey:LecturerID"`
 }
