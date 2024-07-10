2bb99482e0a5cebb297d122e94859ca5367e4854
markmumba <markmumba01@gmail.com>
2024-07-10 11:56:15 +0300

    reverted back to previous commit and re-wrote the submission page

--- frontend/app/dashboard/lecturer/submission/page.tsx
+++ frontend/app/dashboard/lecturer/submission/page.tsx
@@ -3,8 +3,8 @@ import { useRouter } from 'next/navigation';
 import useSWR from 'swr';
 import React, { useState, useEffect } from 'react';
 import { useSubmissionStore } from '@/app/shared/store';
-import { LecturerSubmissionDetails } from '@/app/shared/types';
-import fetcher from '@/app/fetcher/fetcher';
+import { CreateFeedbackFormData, LecturerSubmissionDetails } from '@/app/shared/types';
+import fetcher, { axiosInstance } from '@/app/fetcher/fetcher';
 
 function SubmissionDetail() {
 
@@ -21,7 +21,11 @@ function SubmissionDetail() {
 
     console.log('Submission:', submission);
 
-    const [feedback, setFeedback] = useState<string>('');
+    const [feedback, setFeedback] = useState<CreateFeedbackFormData>({
+        submission_id: selectedSubmissionId,
+        feedback_date: new Date().toISOString(),
+        comments: '',
+    });
 
     if (error) {
         return <div>Error loading submission details</div>;
@@ -35,8 +39,20 @@ function SubmissionDetail() {
         e.preventDefault();
         try {
             const responseJson = JSON.stringify(feedback);
-            console.log('Feedback:', feedback);
-            setFeedback('');
+            console.log('Feedback:', responseJson);
+            const response = await axiosInstance.post('/feedback', responseJson, {
+                withCredentials: true,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            setFeedback({
+                submission_id: selectedSubmissionId,
+                feedback_date: new Date().toISOString(),
+                comments: '',
+            });
+            router.push('/dashboard');
         } catch (error) {
             console.error('Error submitting feedback:', error);
         }
@@ -61,8 +77,8 @@ function SubmissionDetail() {
                     <textarea
                         id="feedback"
                         name="feedback"
-                        value={feedback}
-                        onChange={(e) => setFeedback(e.target.value)}
+                        value={feedback.comments}
+                        onChange={(e) => setFeedback({ ...feedback, comments: e.target.value })}
                         rows={4}
                         className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 sm:text-sm"
                         required
