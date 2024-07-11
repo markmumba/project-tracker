cd42bf3b25f6835286ca70bd3442531778f696bc
markmumba <markmumba01@gmail.com>
2024-07-11 14:01:41 +0300

    added the feedback modal can see the reference submission changed button to take you to all the feedback

--- frontend/app/UI/dashboard/student/feedbacks.tsx
+++ frontend/app/UI/dashboard/student/feedbacks.tsx
@@ -2,41 +2,43 @@ import { useState } from 'react';
 import { formatFeedbackDate, truncateDescription } from '@/app/shared/helper';
 import { FeedbackDetails } from '@/app/shared/types';
 import Link from 'next/link';
+import FeedbackModal from './feedbackmodal';
 
 function Feedbacks({ feedbackDetails }: { feedbackDetails: FeedbackDetails[] | undefined | null }) {
-    const [showRecent, setShowRecent] = useState(true); // State to toggle between recent and previous feedback
+    const [showRecent, setShowRecent] = useState(true);
 
+    const [selectedFeedback, setSelectedFeedback] = useState<FeedbackDetails | null>(null);
     const handleFeedbackClick = (feedback: FeedbackDetails) => {
-        console.log(feedback);
+        setSelectedFeedback(feedback);
+    };
+
+    const closeModal = () => {
+        setSelectedFeedback(null);
     };
 
     // Sort feedback by date in descending order
     const sortedFeedback = feedbackDetails?.slice().sort((a, b) => new Date(b.feedback_date).getTime() - new Date(a.feedback_date).getTime());
 
     // Filter feedback based on the toggle state
-    const filteredFeedback = showRecent
-        ? sortedFeedback?.filter(feedback => feedback.comments !== '') // Example condition for recent feedback
-        : sortedFeedback?.filter(feedback => feedback.comments === ''); // Example condition for previous feedback
 
     return (
         <div className="pt-10">
             <h1 className="text-3xl font-bold">Feedback</h1>
             <div className="flex justify-end mb-4">
                 <button
-                    className={`px-4 py-2 rounded-full focus:outline-none ${showRecent ? 'bg-blue-400 text-white' : 'bg-gray-200 text-gray-700'}`}
+                    className="bg-blue-300  py-2 px-4 rounded-full hover:bg-blue-400 focus:outline-none focus:bg-blue-400"
                     onClick={() => setShowRecent(!showRecent)}
-                >
-                    {showRecent ? 'Show Previous Feedback' : 'Show Recent Feedback'}
-                </button>
+                >View all feedback</button>
             </div>
-            {filteredFeedback?.map((feedback) => (
+
+            {sortedFeedback?.map((feedback) => (
                 <div
                     key={feedback.feedback_id}
                     className="relative pl-8 mb-4 cursor-pointer"
                     onClick={() => handleFeedbackClick(feedback)}
                 >
                     <div className="flex items-center mb-2">
-                        <div className="animate-ping bg-blue-500 h-4 w-4 rounded-full border-2 border-white"></div>
+                        <div className=" bg-blue-500 h-4 w-4 rounded-full border-2 border-white"></div>
                         <div className="ml-4 p-4 bg-gray-100 hover:bg-blue-500 hover:text-white group rounded-lg flex-grow max-w-5xl">
                             <p className="mb-2">
                                 <span className='text-lg font-bold'>Feedback</span>
@@ -51,6 +53,9 @@ function Feedbacks({ feedbackDetails }: { feedbackDetails: FeedbackDetails[] | u
                     </div>
                 </div>
             ))}
+            {selectedFeedback && (
+                <FeedbackModal feedback={selectedFeedback} onClose={closeModal} />
+            )}
         </div>
     );
 }
--- frontend/app/UI/dashboard/student/timeremaining.tsx
+++ frontend/app/UI/dashboard/student/timeremaining.tsx
@@ -1,6 +1,5 @@
 import { ProjectDetails } from "@/app/shared/types";
 
-
 function TimeRemaining({ projectDetails }: { projectDetails: ProjectDetails | null | undefined }) {
 
     function calculateTimeRemaining(projectDetails: ProjectDetails | null | undefined) {
@@ -12,7 +11,6 @@ function TimeRemaining({ projectDetails }: { projectDetails: ProjectDetails | nu
             const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
             const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
           
-
             return { days, hours };
         }
         return null; // Return null if projectDetails is undefined or null
@@ -20,10 +18,15 @@ function TimeRemaining({ projectDetails }: { projectDetails: ProjectDetails | nu
 
     const timeRemaining = calculateTimeRemaining(projectDetails);
 
+    let backgroundColorClass = "bg-blue-300";
+    if (timeRemaining && timeRemaining.days < 20) {
+        backgroundColorClass = "bg-red-500";
+    }
+
     return (
-        <div className="px-4 py-2 ml-4   item-center rounded-full bg-blue-300 max-w-xs focus:outline-none justify-center">
+        <div className={`px-4 py-2 ml-4 rounded-full max-w-xs focus:outline-none justify-center ${backgroundColorClass}`}>
             {timeRemaining && (timeRemaining.days > 0 || timeRemaining.hours > 0 )
-                ? `Time remaining  to presentation : ${timeRemaining.days}d ${timeRemaining.hours}h `
+                ? `Time remaining to presentation: ${timeRemaining.days}d ${timeRemaining.hours}h`
                 : "Time is up"}
         </div>
     );
