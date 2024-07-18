a72e4da34246f45933a6cb14f89360dd9b8e5c66
markmumba <markmumba01@gmail.com>
2024-07-18 21:13:08 +0300

    hiding secret data

--- frontend/app/shared/firebaseconfig.ts
+++ frontend/app/shared/firebaseconfig.ts
@@ -1,16 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
-// TODO: Add SDKs for Firebase products that you want to use
 const firebaseConfig = {
-  apiKey: "AIzaSyAdylr89fILGrjg3QdnV4wp0y8dVH-XClk",
-  authDomain: "projecttracker-3e7f5.firebaseapp.com",
-  projectId: "projecttracker-3e7f5",
-  storageBucket: "projecttracker-3e7f5.appspot.com",
-  messagingSenderId: "597391387587",
-  appId: "1:597391387587:web:ff53730e24dd4bc1672b76",
-  measurementId: "G-9CNXMRZJ9K"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
